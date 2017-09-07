6c4e76775482dd0f7699aab7c31c2165a47c9380
vesto001 <vicestopinan@yahoo.com>
2017-09-07 15:38:32 -0500

    Added request for employees by grade

--- src/App.js
+++ src/App.js
@@ -8,8 +8,38 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      grades: []
+      grades: [],
+      currentGrade: ''
     }
+    this.updateCurrentDate = this.updateCurrentDate.bind(this);
+    this.getEmployeesById = this.getEmployeesById.bind(this);
+  }
+
+  getEmployeesById(){
+    let url = '';
+    if(this.state.currentGrade === 'allGrades'){
+      url = 'http://localhost:4000/api/employees';
+    }
+    else{
+      url = 'http://localhost:4000/api/grades/' + this.state.currentGrade + '/employees';
+    }
+    let employees = [];
+    this.serverRequest = $.ajax({
+         url: url,
+         error: function (e) {
+             console.dir(e);
+         },
+         success: function (data) {
+           console.log(data.employees);
+           employees = data.employees;
+         },
+         async:false
+     });
+  }
+
+  updateCurrentDate(e){
+    console.log(e.target.value);
+    this.setState({currentGrade: e.target.value});
   }
 
   componentDidMount(){
@@ -32,10 +62,10 @@ class App extends Component {
     return (
 
       
-     <div>
+    <div>
        <h2>Select Grades</h2>
 
-        <select>
+        <select onChange={this.updateCurrentDate} >
           <option></option>
           <option value="allGrades">All Grades</option>
           {this.state.grades.map((grade, index)=>{
@@ -44,6 +74,8 @@ class App extends Component {
             );
           })}
         </select>
+
+        <button onClick={this.getEmployeesById}>Submit</button>
     </div>
    
     );
